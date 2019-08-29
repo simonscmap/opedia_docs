49291a186d7a28da2f0826562a0c4f4b9d188303
raphael <norlandrhagen@gmail.com>
2019-08-29 15:50:37 -0700

    added glodap, added cobalamine, added time col to some datasets in catalog

--- conf.py
+++ conf.py
@@ -45,7 +45,8 @@ extensions = [
    'sphinx.ext.coverage',
    'sphinx.ext.mathjax',
    'sphinx.ext.viewcode',
-   'sphinx.ext.githubpages'
+   'sphinx.ext.githubpages'#,
+# 'sphinx_gallery.gen_gallery'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
